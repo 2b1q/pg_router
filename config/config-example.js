fd81d5d23f2f4e8b8fd26a9783bf3c5d5081afb7
2b1q <i.shevtsov@bankex.com>
2018-12-12 12:31:42 +0300

    config

--- config/config-example.js
+++ config/config-example.js
@@ -73,32 +73,28 @@ config.staging = {
         ip: "0.0.0.0"
     },
     btc_main_net_server: {
-        port: 8332,
-        ip: "0.0.0.0"
+        port: 8332
     },
     btc_test_net_server: {
-        port: 18332,
-        ip: "0.0.0.0"
+        port: 18332
     },
     ltc_main_net_server: {
-        port: 9332,
-        ip: "0.0.0.0"
+        port: 9332
     },
     ltc_test_net_server: {
-        port: 19332,
-        ip: "0.0.0.0"
+        port: 19332
     },
     /** ============= NEED TO BE SPECIFIED ============= */
     store: {
         redis: {
-            host: 'localhost', // redis server hostname
-            port: 6379,        // redis server port
-            scope: 'staging'      // use scope to prevent sharing messages between "node redis rpc"
+            host: "localhost", // redis server hostname
+            port: 6379, // redis server port
+            scope: "dev" // use scope to prevent sharing messages between "node redis rpc"
         },
         channel: {
-            jrpc: wid => typeof wid === 'undefined' ? 'pg_jrpc:' : 'pg_jrpc:' + wid,
-            auth: wid => typeof wid === 'undefined' ? 'pg_auth:' : 'pg_auth:' + wid,
-            nm: wid => typeof wid === 'undefined' ? 'pg_nm:' : 'pg_nm:' + wid,
+            jrpc: wid => (typeof wid === "undefined" ? "pg_jrpc:" : "pg_jrpc:" + wid),
+            auth: wid => (typeof wid === "undefined" ? "pg_auth:" : "pg_auth:" + wid),
+            nm: wid => (typeof wid === "undefined" ? "pg_nm:" : "pg_nm:" + wid)
         }
     },
     color: color
@@ -137,39 +133,35 @@ config.dev = {
         ip: "0.0.0.0"
     },
     btc_main_net_server: {
-        port: 8332,
-        ip: "0.0.0.0"
+        port: 8332
     },
     btc_test_net_server: {
-        port: 18332,
-        ip: "0.0.0.0"
+        port: 18332
     },
     ltc_main_net_server: {
-        port: 9332,
-        ip: "0.0.0.0"
+        port: 9332
     },
     ltc_test_net_server: {
-        port: 19332,
-        ip: "0.0.0.0"
+        port: 19332
     },
     /** ============= NEED TO BE SPECIFIED ============= */
     store: {
         redis: {
-            host: 'redis', // redis server hostname
-            port: 6379,        // redis server port
-            scope: 'dev'      // use scope to prevent sharing messages between "node redis rpc"
+            host: "redis", // redis server hostname
+            port: 6379, // redis server port
+            scope: "dev" // use scope to prevent sharing messages between "node redis rpc"
         },
         channel: {
-            jrpc: wid => typeof wid === 'undefined' ? 'pg_jrpc:' : 'pg_jrpc:' + wid,
-            auth: wid => typeof wid === 'undefined' ? 'pg_auth:' : 'pg_auth:' + wid,
-            nm: wid => typeof wid === 'undefined' ? 'pg_nm:' : 'pg_nm:' + wid,
+            jrpc: wid => (typeof wid === "undefined" ? "pg_jrpc:" : "pg_jrpc:" + wid),
+            auth: wid => (typeof wid === "undefined" ? "pg_auth:" : "pg_auth:" + wid),
+            nm: wid => (typeof wid === "undefined" ? "pg_nm:" : "pg_nm:" + wid)
         }
     },
     color: color
 };
 /** END OF Dev environment */
 
-    // Determine passed ENV
+// Determine passed ENV
 const currentEnv = typeof process.env.NODE_ENV == "string" ? process.env.NODE_ENV.toLowerCase() : "";
 
 // Check ENV to export (if ENV not passed => default ENV is 'staging')

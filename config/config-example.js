f888f9b2bf9564a5c63d03dedf6db2da4d29d2d3
2b3q <2bb3qq@gmail.com>
2018-12-12 12:56:14 +0300

    config

--- config/config-example.js
+++ config/config-example.js
@@ -87,7 +87,7 @@ config.staging = {
     /** ============= NEED TO BE SPECIFIED ============= */
     store: {
         redis: {
-            host: "localhost", // redis server hostname
+            host: "redis", // redis server hostname
             port: 6379, // redis server port
             scope: "dev" // use scope to prevent sharing messages between "node redis rpc"
         },
@@ -161,7 +161,7 @@ config.dev = {
 };
 /** END OF Dev environment */
 
-// Determine passed ENV
+    // Determine passed ENV
 const currentEnv = typeof process.env.NODE_ENV == "string" ? process.env.NODE_ENV.toLowerCase() : "";
 
 // Check ENV to export (if ENV not passed => default ENV is 'staging')
